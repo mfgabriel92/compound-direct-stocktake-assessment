ef644514cbf8ebaf35c844766846a9fd4f8c1836
Gabriel Monteiro Fernandes <mfgabriel92@gmail.com>
2024-12-07 18:55:13 +0800

    refactor: same refactor as remaining list

--- src/components/CountedListTable.tsx
+++ src/components/CountedListTable.tsx
@@ -4,7 +4,7 @@ import { HiDotsVertical } from "react-icons/hi";
 import { Table } from ".";
 import { countedList } from "../stores";
 import { renderUnitOrUnitsText } from "../utils";
-import { CountedListStatusIcon, EmptyTable, TableRow } from "./ui";
+import { CountedListStatusIcon, TableRow } from "./ui";
 
 function CountedListTable() {
   const header = [
@@ -20,40 +20,23 @@ function CountedListTable() {
     countedList.getCountedList();
   }, []);
 
-  function renderListOrEmptyMessage() {
-    const { list } = countedList;
-
-    if (!list.length) {
-      return (
-        <tbody>
-          <EmptyTable />
-        </tbody>
-      );
-    }
-
-    return (
-      <tbody>
-        {list.map((record) => (
-          <tr
-            key={record.id}
-            className="h-11 transition-colors hover:bg-gray-50"
-          >
-            <TableRow className="w-[400px]">
-              <a href="#" className="text-blue-500">
-                {record.stockName}
-              </a>
-            </TableRow>
-            <TableRow>{renderUnitOrUnitsText(record.previousQty)}</TableRow>
-            <TableRow>{renderUnitOrUnitsText(record.count)}</TableRow>
-            <TableRow>{renderUnitOrUnitsText(record.movement)}</TableRow>
-            <TableRow>{<CountedListStatusIcon record={record} />}</TableRow>
-            <td className="flex h-11 items-center justify-end">
-              <HiDotsVertical className="cursor-pointer hover:text-gray-900" />
-            </td>
-          </tr>
-        ))}
-      </tbody>
-    );
+  function renderTableRows() {
+    return countedList.list.map((record) => (
+      <tr key={record.id} className="h-11 transition-colors hover:bg-gray-50">
+        <TableRow className="w-[400px]">
+          <a href="#" className="text-blue-500">
+            {record.stockName}
+          </a>
+        </TableRow>
+        <TableRow>{renderUnitOrUnitsText(record.previousQty)}</TableRow>
+        <TableRow>{renderUnitOrUnitsText(record.count)}</TableRow>
+        <TableRow>{renderUnitOrUnitsText(record.movement)}</TableRow>
+        <TableRow>{<CountedListStatusIcon record={record} />}</TableRow>
+        <td className="flex h-11 items-center justify-end">
+          <HiDotsVertical className="cursor-pointer hover:text-gray-900" />
+        </td>
+      </tr>
+    ));
   }
 
   return (
@@ -65,7 +48,7 @@ function CountedListTable() {
           isLoading={countedList.isLoading}
           className="mt-10"
         >
-          {renderListOrEmptyMessage()}
+          {renderTableRows()}
         </Table>
       )}
     </Observer>
