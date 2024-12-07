e9283920783f179a73346f8812e1645a2562b6f5
Gabriel Monteiro Fernandes <mfgabriel92@gmail.com>
2024-12-07 17:19:42 +0800

    refactor: separated rendering of remaining table rows into functions

--- src/components/RemainingListTable.tsx
+++ src/components/RemainingListTable.tsx
@@ -1,25 +1,44 @@
-﻿import { Observer } from "mobx-react-lite";
-import { useEffect } from "react";
+﻿import { toJS } from "mobx";
+import { Observer } from "mobx-react-lite";
+import { useEffect, HTMLAttributes } from "react";
 import { BiEdit } from "react-icons/bi";
 import { Table } from ".";
 import { useModal } from "../contexts";
+import { RemainingListModel } from "../models";
 import { remainingList } from "../stores";
 import { renderUnitOrUnitsText } from "../utils";
 import { EmptyTable, TableRow } from "./ui";
 
 export function RemainingListTable() {
-  const { toggle } = useModal();
+  const { toggleOpenClose, setData } = useModal();
 
-  const header = ["stock name", "current qty", ""];
+  const header = ["Stock Name", "Current Qty", ""];
 
   useEffect(() => {
     remainingList.getRemainingList();
   }, []);
 
-  function renderList() {
-    const { list } = remainingList;
+  function handleToggleOpen(item: RemainingListModel) {
+    toggleOpenClose();
+    setData(toJS(item));
+  }
 
-    if (!list.length) {
+  function renderTableRows() {
+    return remainingList.list.map((item) => (
+      <tr key={item.id} className="h-10 transition-colors hover:bg-gray-50">
+        <TableRow className="w-[400px] text-blue-500">
+          {item.stockName}
+        </TableRow>
+        <TableRow>{renderUnitOrUnitsText(item.currentQty)}</TableRow>
+        <TableRow>
+          <ActionButton onClick={() => handleToggleOpen(item)} />
+        </TableRow>
+      </tr>
+    ));
+  }
+
+  function renderTableBody() {
+    if (!remainingList.list.length) {
       return (
         <tbody>
           <EmptyTable />
@@ -27,29 +46,7 @@ export function RemainingListTable() {
       );
     }
 
-    return (
-      <tbody>
-        {remainingList.list.map((i) => (
-          <tr key={i.id} className="h-10 transition-colors hover:bg-gray-50">
-            <TableRow className="w-[400px]">
-              <a href="#" className="text-blue-500">
-                {i.stockName}
-              </a>
-            </TableRow>
-            <TableRow>{renderUnitOrUnitsText(i.currentQty)}</TableRow>
-            <TableRow>
-              <span
-                className="flex h-11 cursor-pointer items-center justify-end gap-2 text-blue-500"
-                onClick={toggle}
-              >
-                <BiEdit className="-mt-1" />
-                Record Count
-              </span>
-            </TableRow>
-          </tr>
-        ))}
-      </tbody>
-    );
+    return <tbody>{renderTableRows()}</tbody>;
   }
 
   return (
@@ -61,9 +58,21 @@ export function RemainingListTable() {
           isLoading={remainingList.isLoading}
           className="mt-12"
         >
-          {renderList()}
+          {renderTableBody()}
         </Table>
       )}
     </Observer>
+  );
+}
+
+function ActionButton({ onClick }: HTMLAttributes<HTMLSpanElement>) {
+  return (
+    <span
+      className="float-end flex h-11 cursor-pointer items-center justify-end gap-2 text-blue-500 hover:underline"
+      onClick={onClick}
+    >
+      <BiEdit className="-mt-1" />
+      Record Count
+    </span>
   );
 }
